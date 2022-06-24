56b1feb1c9572668c23bfb70b5bce3658b310ae8
Offliners <eric21904@gmail.com>
2022-06-24 19:39:24 +0800

    update example1

--- Example1-Covid19-Cases-Prediction/python/cfg.py
+++ Example1-Covid19-Cases-Prediction/python/cfg.py
@@ -1,18 +1,24 @@
 cfg = {
-    'seed' : 0,
+    'seed' : 5211314,
     'epoch' : 5000,
-    'train_batchsize' : 16,
+    'train_batchsize' : 8,
+    'valid_batchsize' : 8,
+    'test_batchsize' : 8,
     'split_ratio' : 0.2,
-    'select_all' : True,
-    'select_feature' : [],
+    'select_all' : False,
+    'select_features' : [38, 39, 40, 41, 53, 54, 55, 56, 57, 69, 70, 71, 72, 73, 85, 86, 87, 88, 89, 101, 102, 103, 104, 105],
 
-    'optimizer': 'Adam',
+    'optimizer': 'AdamW',
     'optim_hparas': {
-        'lr' : 1e-5,
-        'weight_decay' : 1e-6
+        'lr' : 5e-4,
+        'betas' : (0.9, 0.98)
     },
 
-    'early_stop' : 400,
-    'save_path' : './checkpoints',
-    'tensorboard' : './'
+    'train_path' : './data/train.csv',
+    'test_path' : './data/test.csv',
+
+    'early_stop' : 200,
+    'save_model_path' : './checkpoints/model.pth',
+    'save_csv_path' : './result',
+    'tensorboard' : './tensorboard'
 }
--- Example1-Covid19-Cases-Prediction/python/utils.py
+++ Example1-Covid19-Cases-Prediction/python/utils.py
@@ -1,6 +1,8 @@
+import csv
 import numpy as np
 import torch
-
+from tqdm import tqdm
+from torch.utils.data import random_split
 
 def same_seed(seed): 
     torch.backends.cudnn.deterministic = True
@@ -9,3 +11,44 @@ def same_seed(seed):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+
+
+def select_feature(train_data, valid_data, test_data, features, select_all=True):
+    y_train, y_valid = train_data[:,-1], valid_data[:,-1]
+    raw_x_train, raw_x_valid, raw_x_test = train_data[:,:-1], valid_data[:,:-1], test_data
+
+    if select_all:
+        feat_idx = list(range(raw_x_train.shape[1]))
+    else:
+        feat_idx = features
+        
+    return raw_x_train[:,feat_idx], raw_x_valid[:,feat_idx], raw_x_test[:,feat_idx], y_train, y_valid
+
+
+def train_valid_split(data_set, valid_ratio, seed):
+    valid_set_size = int(valid_ratio * len(data_set)) 
+    train_set_size = len(data_set) - valid_set_size
+    train_set, valid_set = random_split(data_set, [train_set_size, valid_set_size], generator=torch.Generator().manual_seed(seed))
+    
+    return np.array(train_set), np.array(valid_set)
+
+
+def predict(test_loader, model, device):
+    model.eval()
+    preds = []
+    for x in tqdm(test_loader):
+        x = x.to(device)                        
+        with torch.no_grad():                   
+            pred = model(x)                     
+            preds.append(pred.detach().cpu())   
+    preds = torch.cat(preds, dim=0).numpy()  
+    
+    return preds
+
+
+def save_pred(preds, file):
+    with open(file, 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(['id', 'tested_positive'])
+        for i, p in enumerate(preds):
+            writer.writerow([i, p])
